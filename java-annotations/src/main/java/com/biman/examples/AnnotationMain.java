b75ffde60c91c372f6c9ce4e7a16e7265ea6dae3
Biman Mandal <bmandal1@zebra.com>
2019-12-28 13:07:44 +0530

    updated the use of annotations

--- java-annotations/src/main/java/com/biman/examples/AnnotationMain.java
+++ java-annotations/src/main/java/com/biman/examples/AnnotationMain.java
@@ -1,7 +1,25 @@
 package com.biman.examples;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class AnnotationMain {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        new CustomAnnotationsImplementation();
+        Class<CustomAnnotationsImplementation> classObj = CustomAnnotationsImplementation.class;
+        printDetailsAboutAnnotations(classObj.getDeclaredMethods());
+    }
+
+    public static void printDetailsAboutAnnotations(Method[] methods){
+        for (Method method: methods){
+            System.out.println(method.getName()+ "===");
+            for (Annotation annotation: method.getAnnotations()){
+                if (annotation instanceof CustomAnnotations){
+                    CustomAnnotations customAnnotations = (CustomAnnotations) annotation;
+                    System.out.println(customAnnotations.value());
+                }
+            }
+        }
     }
 }
